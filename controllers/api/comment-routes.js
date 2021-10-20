858da99f6c7b427919479dfedb28116efc08eab9
zugzw4ng <bongcloud2001@gmail.com>
2021-10-19 20:35:05 -0400

    added POST comment route

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -17,3 +17,18 @@ router.get('/', (req, res) => {
             res.status(400).json(err);
         });
 });
+
+router.post('/', withAuth, (req, res) => {
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id
+        })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
+});
