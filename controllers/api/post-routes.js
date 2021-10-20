443367ea21453a3194260c0b14134fa4d80d8b80
zugzw4ng <bongcloud2001@gmail.com>
2021-10-19 20:40:28 -0400

    added GET post by ID route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -32,3 +32,41 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'Post not found.' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
