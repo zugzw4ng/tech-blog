519f6df5d2ebdfbef0784aa871569f0ad5798659
zugzw4ng <bongcloud2001@gmail.com>
2021-10-19 22:58:44 -0400

    added comment seed data

---
+++ seeds/commentSeedData.js
@@ -0,0 +1,18 @@
+[
+    {
+      "content": "Test. Test. Test.",
+      "blogpost_id": 1
+    },
+    {
+      "content": "Long Time Listener First Time Caller:",
+      "blogpost_id": 1
+    },
+    {
+      "content": "lol",
+      "blogpost_id": 2
+    },
+    {
+      "content": "This is already outdated.",
+      "blogpost_id": 3
+    }
+]
