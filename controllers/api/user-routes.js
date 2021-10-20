ae387b749991af9a52d0ca24d3b12eef089fede3
zugzw4ng <bongcloud2001@gmail.com>
2021-10-19 20:29:41 -0400

    added POST create User route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -74,3 +74,20 @@ router.post('/login', (req, res) => {
         });
     });
 });
+
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        });
+});
