08b16e96ee269473a5d96eca056d73cab0f1ec25
zugzw4ng <bongcloud2001@gmail.com>
2021-10-19 22:57:11 -0400

    added post seed data

---
+++ seeds/postSeedData.js
@@ -0,0 +1,17 @@
+[
+    {
+      "title": "High Tech, Higher Tech, Highest Tech, From Turing to Turing",
+      "content": "The history of computing power",
+      "user_id": 1
+    },
+    {
+      "title": "Autobiography of Nothing",
+      "content": "Null",
+      "user_id": 4
+    },
+    {
+      "title": "Sequelize",
+      "content": "It's Great. Powered by Node",
+      "user_id": 2
+    }
+]
