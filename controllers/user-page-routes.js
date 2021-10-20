3701c1e4d0876e099fd30930d385f0f912e10af5
zugzw4ng <bongcloud2001@gmail.com>
2021-10-19 21:17:26 -0400

    added GET user post to edit route

--- controllers/user-page-routes.js
+++ controllers/user-page-routes.js
@@ -38,3 +38,49 @@ router.get('/', withAuth, (req, res) => {
         });
 });
 
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'Post not found.' });
+                return;
+            }
+
+            const post = dbPostData.get({ plain: true });
+
+            res.render('edit-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
+
