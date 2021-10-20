e2b258e2c3b30679188396d62655e9ad6ca03338
zugzw4ng <bongcloud2001@gmail.com>
2021-10-19 20:25:02 -0400

    added GET users by ID route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote, Comment } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
@@ -7,6 +7,40 @@ router.get('/', (req, res) => {
         attributes: { exclude: ['password'] }
     })
         .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            },
+        ]
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this ID' });
+                return;
+            }
+            res.json(dbUserData);
+        })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
