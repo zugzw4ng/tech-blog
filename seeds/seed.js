5191c55667d4c7f386fea9034fcba51d2b39fe08
zugzw4ng <bongcloud2001@gmail.com>
2021-10-31 05:28:03 -0400

    refactored handlebars added login handlebars

--- seeds/seed.js
+++ seeds/seed.js
@@ -14,7 +14,7 @@ const seedDatabase = async () => {
 
   for (comment of commentSeedData) {
     const newComment = await Comment.create({
-      ...Comment,
+      ...comment,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
